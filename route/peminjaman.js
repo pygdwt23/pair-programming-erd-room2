1221f448e681212ee52148b3d442ba8f8bb554ab
yogo <pygdwt@gmail.com>
2021-02-06 19:36:50 +0800

    [Yogo] Final

--- route/peminjaman.js
+++ route/peminjaman.js
@@ -2,20 +2,20 @@ const express = require("express");
 
 const route = express();
 
-const dbController = require('./../controller/db')
+const dbController = require("./../controller/db");
 
 //GET
-route.get("/api/v1/peminjaman",dbController.getAPIPeminjaman );
+route.get("/api/v1/peminjaman", dbController.getAPIPeminjaman);
 
-route.get("/api/v1/peminjaman/:id",dbController.getAPIByIdPeminjaman);
+route.get("/api/v1/peminjaman/:id", dbController.getAPIByIdPeminjaman);
 
 //POST
-route.post("/api/v1/peminjaman",dbController.postAPIPeminjaman );
+route.post("/api/v1/peminjaman", dbController.postAPIPeminjaman);
 
 //PUT
-route.put("/api/v1/peminjaman/:id",dbController.putAPIPeminjaman );
+route.put("/api/v1/peminjaman/:id", dbController.putAPIPeminjaman);
 
 //DELETE
-route.delete("/api/v1/peminjaman/:id",dbController.deleteAPIPeminjaman );
+route.delete("/api/v1/peminjaman/:id", dbController.deleteAPIPeminjaman);
 
 module.exports = route;
